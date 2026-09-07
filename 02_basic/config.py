d60a5f64bf180e7368cbe303024681715f1cfafd
skyup-509 <parkcs0509@gmail.com>
2026-09-07 14:04:27 +0900

    config 수정

--- 02_basic/config.py
+++ 02_basic/config.py
@@ -2,17 +2,6 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-try:
-    from google.colab import userdata
-
-    if not os.getenv("GITHUB_TOKEN"):
-        token = userdata.get("GITHUB_TOKEN")
-        if token:
-            os.environ["GITHUB_TOKEN"] = token
-
-except ImportError:
-    pass
-
 # 파일 불러오는 url 설정
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 
